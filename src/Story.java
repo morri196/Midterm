a2a2e8f6b5db5b09b3a2ade671b70026bc511301
foleya <andrew@clicktutor.io>
2018-08-02 10:23:27 -0400

    third sentence

--- src/Story.java
+++ src/Story.java
@@ -1,10 +1,14 @@
 
 public class Story {
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 
 		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt"
 				+ " an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println(" All the monkey's were eating bananas. ");
+
+		// Dewey added this!
+		System.out.println("When suddenly they were attacked by crows that wanted to eat the bananas!");
+
 	}
-	
+
 }
