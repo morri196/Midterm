9d9b474917c9e4bb7a645ba2857377d33ce25a25
Kari Mcfarlin <karimcfarlin@Karis-MacBook-Air.local>
2018-08-02 10:44:52 -0400

    location change

--- src/Story.java
+++ src/Story.java
@@ -2,7 +2,7 @@
 public class Story {
 	public static void main(String[] args) {
 
-		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt"
+		System.out.println("Once upon a time in the mystical land of Downtown Detroit there dwelt"
 				+ " an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		//Amna added this 
 		System.out.println(" All the monkey's were eating bananas. ");
