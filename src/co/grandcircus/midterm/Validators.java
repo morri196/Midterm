fa6b9db6ea647e737f8dea482056bd7200f74dad
foleya <andrew@clicktutor.io>
2018-08-03 15:42:37 -0400

    added alllllll the validators

--- src/co/grandcircus/midterm/Validators.java
+++ src/co/grandcircus/midterm/Validators.java
@@ -1,51 +1,77 @@
 package co.grandcircus.midterm;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Validators {
 
 	// Get Integers
 
 	// Validate that an integer is within range 0 - maxNum
-	public static void isInRange(int num, int maxNum) {
-		if (num < 1 || num > maxNum) {
+	public static void isInRange(int userInput, int maxNum) {
+		if (userInput < 1 || userInput > maxNum) {
 			throw new IllegalArgumentException("You must enter a number (1-" + maxNum + ")! Please try again:");
 		}
-
 	}
 
 	// Validate that an integer is positive
-	public static void isPositive(int num) {
-		if (num <= 0) {
-			throw new IllegalArgumentException("You must enter a positive number.");
+	public static void isPositive(int userInput) {
+		if (userInput <= 0) {
+			throw new IllegalArgumentException("You must enter a positive number. Please try again:");
 		}
-
 	}
 
 	// Validate that a double is positive
-	public static void isPositive(double num) {
-		if (num <= 0) {
-			throw new IllegalArgumentException("You must enter a positive number.");
+	public static void isPositive(double userInput) {
+		if (userInput <= 0) {
+			throw new IllegalArgumentException("You must enter a positive number. Please try again:");
 		}
-
 	}
 
-	// Get Menu Choice
+	public static void isCorrectNumberOfDigits(int userInput, int correctNumDigits) {
+		if (String.valueOf(userInput).length() != correctNumDigits) {
+			throw new IllegalArgumentException("You must enter " + correctNumDigits + " digits. Please try again:");
+		}
+	}
 
-	// Get Quantity
-
-	// Get Check Number
-
-	// Get Credit Card CVV
-
-	// Get Double
-
-	// Get Cash
-
-	// Get Something Weird (REGEX?)
-
-	// Get Credit Card Number
-
-	// Get Date
-
-	// Get Credit Card Exp
+	public static void isGreaterThanMinAmount(double userInput, double minAmount) {
+		if (userInput < minAmount) {
+			throw new IllegalArgumentException("You must enter at least $" + minAmount + ". Please try again:");
+		}
+	}
+	
+	public static void isValidNameOnCreditCard(String userInput) {
+		if (!userInput.matches("^(?!.*  )[a-zA-Z ]*$")) {
+			throw new IllegalArgumentException("You must enter your name as it appears on the card. Please try again:");
+		}
+	}
+	
+	public static void isValidCreditCardNumber(String userInput) {
+		if (!userInput.matches("^\\d{4}-\\d{4}-\\d{4}-\\d{4}$")) {
+			throw new IllegalArgumentException(
+					"custom yell at them message");
+		}
+	}
+	
+	public static void isValidCreditCardExpirationDate(String userInput) {
+		DateFormat format = new SimpleDateFormat("MM/yy");
+		format.setLenient(false);		/* Inputs must match specified format */
+		try {
+			// Try to parse a date (non-leniently) from the input
+			format.parse(userInput);
+		} catch (ParseException pe) {
+			throw new IllegalArgumentException("Date format must be MM/YY. Please try again:");
+		}
+		
+		// Check for dates that could be parsed but do not follow dd/mm/yyyy format
+		// For example, 01/01/12345 could be successfully parsed, but it does not
+		// match the specified format.
+		// MIGHT BE OVERKILL?
+		if (!userInput.matches("^[0-9]{2}/[0-9]{2}$")) {
+			throw new IllegalArgumentException("Date format must be MM/YY. Please try again:");
+		}
+		
+	}
 
 }
--- src/co/grandcircus/midterm/Vtest.java
+++ src/co/grandcircus/midterm/Vtest.java
@@ -6,14 +6,25 @@ public class Vtest {
 
 	public static void main(String[] args) {
 		
+		// THIS IS JUST A DUMMY CLASS FOR TESTING VALIDATORS
+		
 		Scanner scnr = new Scanner(System.in);
 		
 		try {
-			int num = Integer.parseInt(scnr.nextLine());
-			Validators.isInRange(num, 10);
+			System.out.println("Enter mm/yy");
+			String userInput = scnr.nextLine();
+			// Validators.isPositive(userInput);
+			// Validators.isInRange(userInput, 100);
+			// Validators.isCorrectNumberOfDigits(userInput, 1);
+			// Validators.isGreaterThanMinAmount(userInput, 2.75);
+			// Validators.isValidCreditCardNumber(userInput);
+			// Validators.isValidNameOnCreditCard(userInput);
+			Validators.isValidCreditCardExpirationDate(userInput);
 		} catch (IllegalArgumentException ex) {
 			System.out.println(ex.getMessage());
 		}
+		
+		scnr.close();
 		
 	}
 
